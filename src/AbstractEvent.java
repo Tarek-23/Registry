a1e425940ca1249ac6313f834fe7b485cec58810
Tarek Radwan <tarek.23@auceypt.edu>
2019-10-15 15:58:35 +0200

    Initialised repository and created AbstractEvent class.

---
+++ src/AbstractEvent.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public abstract class AbstractEvent {
+
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public abstract void init();
+
+    public abstract boolean hasMoreOccurences();
+
+    public abstract Date nextOccurence();
+}
